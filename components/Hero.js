49b4c3bc2eacd512a89ca81264170fd07f8d89f8
sulphurninja <sulphurninja@gmail.com>
2023-02-11 06:54:43 +0530

    Final

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,7 @@
 import React, { useMemo } from "react";
 import Image from "next/image";
 import ButtonPrimary from "./misc/ButtonPrimary";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import getScrollAnimation from "../utils/getScrollAnimation";
 import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
 
@@ -22,7 +22,7 @@ const Hero = ({
       number: "Chakan Ph 2 MIDC",
       icon: "/assets/Icon/gridicons_location.svg",
     },
-  
+
   ],
 }) => {
   const scrollAnimation = useMemo(() => getScrollAnimation(), []);
@@ -33,32 +33,32 @@ const Hero = ({
       id="about"
     >
       <ScrollAnimationWrapper>
-          <motion.div
-            className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16"
-            variants={scrollAnimation}>
-            <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
-              <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
-                Welcome to Nagad Narayan Jogeshwari Industries
-              </h1>
-              <p className="text-black-500 mt-4 mb-6">
-             
-Our industry is representing metal business, such as trading of aluminium scrap, manufacture of alloy ingots and all types of metal business.
-              </p>
-            
-            </div>
-            <div className="flex w-full">
-              <motion.div className="h-full w-full" variants={scrollAnimation}>
-                <Image
-                  src="/assets/Illustration1.png"
-                  alt="VPN Illustrasi"
-                  quality={100}
-                  width={612}
-                  height={383}
-                  layout="responsive"
-                />
-              </motion.div>
-            </div>
-          </motion.div>
+        <motion.div
+          className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16"
+          variants={scrollAnimation}>
+          <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
+            <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
+              Welcome to Nagad Narayan Jogeshwari Industries
+            </h1>
+            <p className="text-black-500 mt-4 mb-6">
+
+              Our industry is representing metal business, such as trading of aluminium scrap, manufacture of alloy ingots and all types of metal business.
+            </p>
+
+          </div>
+          <div className="flex w-full">
+            <motion.div className="h-full w-full" variants={scrollAnimation}>
+              <Image
+                src="/assets/Illustration1.png"
+                alt="VPN Illustrasi"
+                quality={100}
+                width={612}
+                height={383}
+                layout="responsive"
+              />
+            </motion.div>
+          </div>
+        </motion.div>
       </ScrollAnimationWrapper>
       <div className="relative w-full flex">
         <ScrollAnimationWrapper
@@ -67,7 +67,7 @@ Our industry is representing metal business, such as trading of aluminium scrap,
             <motion.div
               className="flex items-center justify-start sm:justify-center py-4 sm:py-6 w-8/12 px-4 sm:w-auto mx-auto sm:mx-0"
               key={index}
-              custom={{duration: 2 + index}}
+              custom={{ duration: 2 + index }}
               variants={scrollAnimation}
             >
               <div className="flex mx-auto w-40 sm:w-auto">
@@ -83,11 +83,11 @@ Our industry is representing metal business, such as trading of aluminium scrap,
               </div>
             </motion.div>
           ))}
-       </ScrollAnimationWrapper>
-       <div
+        </ScrollAnimationWrapper>
+        <div
           className="absolute bg-black-600 opacity-5 w-11/12 roudned-lg h-64 sm:h-48 top-0 mt-8 mx-auto left-0 right-0"
           style={{ filter: "blur(114px)" }}
-       ></div>
+        ></div>
       </div>
     </div>
   );
